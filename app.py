cc19e6f438a53a871720a08a2f700059abd2ec48
Marina-Riv <mar.riv.sci@outlook.com>
2024-06-05 17:45:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv')
+vehicle_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma')
 
 if hist_button:
     st.write('Creación de un histograma para los modelos disponibles en venta')
-    fig = px.histogram(car_data, x="model")
+    fig = px.histogram(vehicle_data, x="model")
     st.plotly_chart(fig, use_container_width=True)
 
 
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram:
-    st.write('Construir un histograma para la columna model')    
+    st.write('Construir un histograma para la condicion de los vehiculos')
+    fig = px.histogram(vehicle_data, x="condition")
+    st.plotly_chart(fig, use_container_width=True)  
